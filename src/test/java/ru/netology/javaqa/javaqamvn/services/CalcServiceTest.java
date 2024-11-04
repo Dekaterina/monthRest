b2ecf344d24e58838569d7bd85331a52f65e2067
Ekaterina Romanova <Dekaterina83@mail.ru>
2024-11-04 09:08:44 +0300

    CalcServiceTest changed

--- src/test/java/ru/netology/javaqa/javaqamvn/services/CalcServiceTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/CalcServiceTest.java
@@ -1,20 +1,24 @@
 package ru.netology.javaqa.javaqamvn.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-//import org.example.CalcService;
 
-@Test1
-public void shouldCalculateForMonthOfRest() {
+//import org.example.CalcService;
+public class CalcServiceTest {
+
+    @Test
+    public void shouldCalculateForMonthOfRest() {
         CalcService service = new CalcService();
         int expected = 3;
         int actual = service.calculate(10000, 3000, 20000);
         Assertions.assertEquals(expected, actual);
     }
 
-@Test2
-public void shouldCalculateForMonthOfRestForLargeParams() {
-    CalcService service = new CalcService();
-    int expected = 2;
-    int actual = service.calculate(100000, 60000, 150000);
-    Assertions.assertEquals(expected, actual);
+    @Test
+    public void shouldCalculateForMonthOfRestForLargeParams() {
+        CalcService service = new CalcService();
+        int expected = 2;
+        int actual = service.calculate(100000, 60000, 150000);
+        Assertions.assertEquals(expected, actual);
+    }
 }
